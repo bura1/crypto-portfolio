cc71dbd4ad0b18fdec22b172f1ac92a08001a783
Tomislav <tomislavburic6@gmail.com>
2022-08-09 20:55:53 +0200

    csv loader fix

--- src/Service/CsvLoader.php
+++ src/Service/CsvLoader.php
@@ -4,10 +4,10 @@ namespace Service;
 
 class CsvLoader
 {
-    public function getData($file, $length = 1000, $separator = ',')
+    public function getData($file, $length = 1000, string $separator = ',', string $enclosure = "\"", string $escape = "\\")
     {
         $handle = fopen($file, 'rb');
-        $data = fgetcsv($handle, $length, $separator);
+        $data = fgetcsv($handle, $length, $separator, $enclosure, $escape);
         fclose($handle);
 
         return $data;
