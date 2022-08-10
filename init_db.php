e27986c5df5877d5381a975b49d17cf36c763607
Tomislav <tomislav@krikstudio.com>
2022-08-10 23:02:38 +0200

    init table transactions

--- init_db.php
+++ init_db.php
@@ -5,10 +5,11 @@ use Service\CsvLoader;
 require __DIR__.'/bootstrap.php';
 
 const DB_NAME = 'portfolio';
+const CSV_FILE = 'files/tickers.csv';
 
 $db = createDatabase(DB_NAME);
 createTables($db);
-$coins = getCoinsFromCsvFile();
+$coins = getCoinsFromCsvFile(CSV_FILE);
 insertData($db, $coins);
 
 // Helper functions
@@ -25,20 +26,28 @@ function createDatabase($dbName)
 
 function createTables($dbi)
 {
-    $dbi->exec('DROP TABLE IF EXISTS coin;');
+    $dbi->exec('DROP TABLE IF EXISTS coins;');
 
     $dbi->exec(
-        'CREATE TABLE coin (
+        'CREATE TABLE coins (
         id INTEGER PRIMARY KEY,
         name TEXT NOT NULL,
         symbol TEXT NOT NULL)'
     );
+
+    $dbi->exec(
+        'CREATE TABLE transactions (
+        id INTEGER PRIMARY KEY,
+        symbol TEXT NOT NULL,
+        value TEXT NOT NULL,
+        FOREIGN KEY(symbol) REFERENCES coins(symbol))'
+    );
 }
 
-function getCoinsFromCsvFile()
+function getCoinsFromCsvFile($csvFile)
 {
     $csvLoader = new CsvLoader();
-    return $csvLoader->getData('files/tickers.csv');
+    return $csvLoader->getData($csvFile);
 }
 
 function insertData($dbi, $allCoins)
@@ -46,7 +55,7 @@ function insertData($dbi, $allCoins)
     $coinsString = getStringOfCoins($allCoins);
 
     $dbi->exec(
-        'INSERT INTO "coin" ("name", "symbol") VALUES ' . $coinsString
+        'INSERT INTO "coins" ("name", "symbol") VALUES ' . $coinsString
     );
 }
 
